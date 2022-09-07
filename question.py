aae776c52d6d1440463da25bcf7a274687fa8aa2
Deepak <deepaknnayak34@gmail.com>
2022-09-07 15:00:21 +0530

    assignment8

---
+++ question.py
@@ -0,0 +1,106 @@
+# TODO:1. Write a python script to print MySirG 5 times on the screen
+# i=1
+# while i<=5:
+#   print('MySirG')
+#   i+=1
+# print()
+
+#TODO: 2. Write a python script to print first 10 natural numbers
+# n=1
+# while n<=10:
+#   print(n)
+#   n+=1
+# print()
+
+# TODO:3. Write a python script to print first 10 natural numbers in reverse order
+# n=1
+# while n<=10:
+#   print(11-n)
+#   n=n+1
+# print()
+
+# n=10
+# while n>=1:
+#   print(n)
+#   n-=1
+# print()
+
+#TODO: Write a python script to print first 10 odd natural numbers
+# n = 1
+# while n<=20:
+#   if n%2!=0:
+#     print(n)
+#   n=n+1
+# print()
+
+# i=1
+# n = int(input('Enter the number:'))*2
+# while i<=n:
+#   if i % 2 !=0:
+#     print(i)
+#   i=i+1
+# print()
+
+#TODO: 5. Write a python script to print first 10 odd natural numbers in reverse order
+
+# i=1
+# n= int(input('Enter the number:'))*2
+# while i<=n:
+#   if i % 2 !=0:
+#     print(20-i)
+#   i=i+1
+# print()
+  
+# i=1
+# n= int(input('Enter the number:'))*2
+# while n>=1:
+#   if n%2 !=0:
+#     print(n)
+#   n-=1
+# print()
+
+# TODO:Write a python script to print first 10 even natural numbers
+# i=1
+# n = int(input('Enter the number:'))*2
+# while i<=n:
+#   if(i % 2 == 0):
+#     print(i)
+#   i=i+1
+# print()
+
+
+#TODO: 7. Write a python script to print first 10 even natural numbers in reverse order
+# i=1
+# n = int(input('Ente the number:'))*2
+# while n>=i:
+#   if n % 2 ==0:
+#     print(n)
+#   n=n-1
+# print()
+  
+
+
+#TODO:8. Write a python script to print squares of first 10 natural numbers
+# i=1
+# n = int(input('Enter the number:'))*2
+# while i<=n:
+#   print(i,":",i**2)
+#   i=i+1
+# print()
+
+#TODO:Write a python script to print cubes of first 10 natural numbers
+# i=1
+# n = int(input('Enter the number:'))*2
+# while i<=n:
+#   if i%2 ==0:
+#     print(i,":",i**3)
+#   i=i+1
+# print()
+
+
+#TODO:Write a python script to print first 10 multiples of 5
+i=1
+while i<=10:
+  print(i * 5)
+  i=i+1
+print()
